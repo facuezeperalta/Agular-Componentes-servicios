24c26c4283ec56a144a6c5189ed18aa798b2eac2
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2023-05-24 00:47:25 -0300

    Fin primera clase de componentes

--- mi-tienda/src/app/components/img/img.component.ts
+++ mi-tienda/src/app/components/img/img.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({ /* decorador  le dicen al compilador como debe comportarse*/
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.css']
