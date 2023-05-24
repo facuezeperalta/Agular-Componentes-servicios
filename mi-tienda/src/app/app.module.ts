f930cd504ae932288018e833e6ab5be388e51538
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2023-05-24 00:42:19 -0300

    Creado componente img

--- mi-tienda/src/app/app.module.ts
+++ mi-tienda/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImgComponent } from './components/img/img.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    ImgComponent
+    ImgComponent,
+
   ],
   imports: [
     BrowserModule,
