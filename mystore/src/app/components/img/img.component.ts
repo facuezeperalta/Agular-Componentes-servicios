acd324c91144a64325c2f4262318cd82f4a34bcd
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2023-06-12 18:37:07 -0300

    inputs fixed

--- mystore/src/app/components/img/img.component.ts
+++ mystore/src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./img.component.css']
 })
 export class ImgComponent {
-
+  @Input() img: string = '';
 }
