0a0eb010019050c3284a8aff659e021011ed80e6
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2023-06-13 18:49:10 -0300

    model product done, product component done, app comp uses product

--- mystore/src/app/app.component.ts
+++ mystore/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import{Product} from './models/product.model'
+
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,33 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'mystore';
   imgParent = 'asdsad';
+
+  products: Product[] =[
+     {
+      id : '1',
+      name: 'producto 1',
+      image: '../assets/xbox blue controler.jpg',
+      price: 60
+    },
+    {
+      id : '2',
+      name: 'producto 2',
+      image: '../assets/XboxControlerWhite.jpg',
+      price: 60
+    },
+    {
+      id : '3',
+      name: 'producto 3',
+      image: '../assets/xboxGreenControler.jpg',
+      price: 60
+    },
+    {
+      id : '4',
+      name: 'producto 4',
+      image: '../assets/SonyA7III.jpg',
+      price: 1400
+    }
+  ]
 
   onLoaded(img: string){
     console.log('Log Padre',img);
